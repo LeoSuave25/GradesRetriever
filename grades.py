1f7af6bccc7be8c3f185280cb6101819315c52cc
LeoSuave25 <suaverdez90@gmail.com>
2023-04-27 17:01:09 +0800

    Name Indexing and Print Result Done

--- grades.py
+++ grades.py
@@ -5,6 +5,12 @@ with open('grades.txt', 'r') as file:
     raw_grades = [float(line.split()[1]) for line in student_lines]
 # Find the highest grade and the corresponding student name
 highest_grade = max(raw_grades)
+highest_grade_index = raw_grades.index(highest_grade)
+with open('grades.txt', 'r') as file:
+    lines = file.readlines()[2:]  # Skip the header rows
+    highest_grade_student_name = lines[highest_grade_index].split()[0]
 # Print the result
+print("\033[32m" + f"The highest grade is {highest_grade} and it belongs to {highest_grade_student_name}" + "\033[0m")
 # Checking Error
-print(highest_grade)
+# print(highest_grade)
+# print(highest_grade_student_name)
