fdab09b046c8475bdc2ac6ccfa89a4d02f6b02eb
LeoSuave25 <suaverdez90@gmail.com>
2023-04-27 16:40:16 +0800

    Reading File done and Finding the Highest Grade done

--- grades.py
+++ grades.py
@@ -1,4 +1,10 @@
 # Style the Project
 # Open the file and read the grades
+with open('grades.txt', 'r') as file:
+    student_lines = file.readlines()[2:]  # Skip the header rows
+    raw_grades = [float(line.split()[1]) for line in student_lines]
 # Find the highest grade and the corresponding student name
+highest_grade = max(raw_grades)
 # Print the result
+# Checking Error
+print(highest_grade)
